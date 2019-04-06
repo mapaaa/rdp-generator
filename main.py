f4ab2071667b68078e8a8f482a3822ad9d681b5a
Maria Pandele <maria.pandele33@gmail.com>
2019-04-06 21:38:23 +0300

    Compute first and follow

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def parse_file(f):
         'N': [],
         'Sigma': [],
         'S': 'S',
-        'P': {} 
+        'P': [] 
     }
     for i in range(3):
         line = f.readline()
@@ -27,12 +27,54 @@ def parse_file(f):
         idx = line.find(' -> ')
         left_hand_side = line[:idx]
         right_hand_side = line[idx+4:]
-        if left_hand_side in grammar['P']:
-            grammar['P'][left_hand_side].append(right_hand_side)
-        else:
-            grammar['P'][left_hand_side] = [right_hand_side]
+        grammar['P'].append((left_hand_side, right_hand_side))
     return grammar
-    
+
+
+# based on https://medium.com/100-days-of-algorithms/day-93-first-follow-cfe283998e3e
+def compute_first_follow(grammar):
+    first = {}
+    follow = {}
+    for terminal in grammar['Sigma']:
+        first[terminal] = {terminal}
+        follow[terminal] = set()
+
+    for nonterminal in grammar['N']:
+        first[nonterminal] = set()
+        follow[nonterminal] = set()
+        
+    follow[grammar['S']].add('$')
+    epsilon = set()
+    for left, right in grammar['P']:
+        if right == '$':
+            epsilon.add(left)
+    ok = True
+    while ok == True:
+        ok = False
+
+        for left, right in grammar['P']:
+            for symbol in right:
+                ok |= union(first[left], first[symbol])
+                if symbol not in epsilon:
+                    break
+                else:
+                    ok |= union(epsilon, {left})
+
+            new = follow[left]
+            for symbol in reversed(right):
+                ok |= union(follow[symbol], new)
+                if symbol in epsilon:
+                    new = new.union(first[symbol])
+                else:
+                    new = first[symbol]
+    return first, follow, epsilon
+
+
+def union(first, begins):
+    n = len(first)
+    first |= begins
+    return len(first) != n
+
 
 def main():
     parser = argparse.ArgumentParser(description='Tool for generating a recursive descent parser.')
@@ -46,7 +88,10 @@ def main():
     f = open(inputFile, 'r')
     grammar = parse_file(f)
     f.close()
-    print(grammar)
+    first, follow, epsilon = compute_first_follow(grammar)
+    print(first)
+    print(follow)
+    print(epsilon)
 
 
 if __name__ == '__main__':
